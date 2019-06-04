471d54ce8ab79d2fa9e86bd702178e951dd09480
CorgigamingYT <43857110+CorgigamingYT@users.noreply.github.com>
2019-06-04 14:09:16 +0100

    Embermane done

--- dauntless database V2.py
+++ dauntless database V2.py
@@ -1,4 +1,5 @@
 import time
+import os
 def pp (text,pause):
     print(text)
     time.sleep(pause)
@@ -27,14 +28,54 @@ elif choice1 == "embermane":
             pp("So, the Lesser Embermane is the weakest behemeoth and the first thing you fight.",4)
             pp("""It's stats are as follows:
 Element type: Blaze (Fire)
-Danger level: 1 out of 12
-Recommended armour: Charrogg, Embermane or Hellion.
-Recommended weapon: Boreus, Pangar or Skraev.
+Danger level: 1
+Recommended armour type: Charrogg, Embermane or Hellion.
+Recommended weapon type: Boreus, Pangar or Skraev.
 Recommended weapon power level: 50
 Recommended armour power level: 50
 """,10)
+            yorn = input("Do you want toi learn about something else? ")
+            if yorn == "yes" or "Yes":
+                os.startfile("dauntless databaseV2.py")
+                exit()
+            if yorn == "no" or "No":
+                pp("Thanks for using this code",2)
+                exit()
         if emberb == "2":
-            pp("So the normal embermane, 
+            pp("So the normal embermane, it is tougher to take down than it's lesser brother and much more dangerous.",4)
+            pp("""It's stats are as follows:
+Element type: Blaze (Fire)
+Danger level: 5
+Recommended armour type: Charrogg, Embermane or Hellion.
+Recommended weapon type: Boreus, Pangar or Skraev.
+Recommended weapon power level: 200
+Recommended armour power level: 200
+""",10)
+            yorn = input("Do you want toi learn about something else? ")
+            if yorn == "yes" or "Yes":
+                os.startfile("dauntless databaseV2.py")
+                exit()
+            if yorn == "no" or "No":
+                pp("Thanks for using this code",2)
+                exit()
+        if emberb == "3":
+            pp("So the bloodfire embermane, the toughest type of embermane.",3)
+            pp("""It's stats are as follows,
+Element type: Blaze (Fire)
+Danger level: 10 (Normal), 15 (Heroic)
+Recommended armour type: Charrogg, Embermane or Hellion.
+Recommended weapon type: Boreus, Pangar or Skraev.
+Recommended weapon power level: 350 (Normal), 475 (Heroic)
+Recommended armour power level: 350 (Normal), 475 (Heroic)
+""",10)
+            yorn = input("Do you want toi learn about something else? ")
+            if yorn == "yes" or "Yes":
+                os.startfile("dauntless databaseV2.py")
+                exit()
+            if yorn == "no" or "No":
+                pp("Thanks for using this code",2)
+                exit()
+#Embermane Done
 elif choice1 == "hellion":
      print("c")
 elif choice1 == "boreus":
